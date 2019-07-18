f6123e375199ef2a99893535f7f30ffc7fa6fa4f
karthik_collect.chat <karthikvillanchira@gmail.com>
2019-07-18 23:53:08 +0530

    add hide/show buttons, save text

--- src/index.js
+++ src/index.js
@@ -2,43 +2,77 @@ import Quill from 'quill';
 import './quill.css';
 import './style.css';
 
+function createTextEditorBtn() {
+  const button = document.createElement('button');
+  button.textContent = 'Show editor';
+  button.id = 'showEditor'
+  button.onclick = function () {
+    document.getElementById('parent').style.display = 'block';
+    document.getElementById('closeEditor').style.display = 'block';
+    button.style.display = 'none';
+  }
+  return button
+}
+
+function closeTextEditorBtn() {
+  const button = document.createElement('button');
+  button.textContent = 'Save editor';
+  button.id = 'closeEditor'
+  button.style.display = 'none';
+  button.onclick = function () {
+    document.getElementById('parent').style.display = 'none';
+    document.getElementById('showEditor').style.display = 'block';
+    button.style.display = 'none';
+  }
+  return button
+}
+
+
+function createEditor() {
+  const parent = document.createElement('div');
+  const div = document.createElement('div');
+  parent.id = 'parent'
+  parent.classList.add("parent")
+  div.id = 'editor';
+  parent.style.display = 'none';
+  parent.appendChild(div);
+  return parent;
+}
+
 // Could break if GitHub changes its markup
 function init() {
-    //   const details = document.createElement('details');
-    //   details.classList.add('position-relative', 'js-dropdown-details');
-    //   details.style.userSelect = 'none';
-    //   const summary = document.createElement('summary');
-    //   summary.classList.add('btn', 'btn-sm');
-    //   summary.innerText = 'Filter';
-    //   const container = document.createElement('div');
-    //   container.classList.add('dropdown-menu', 'dropdown-menu-se', 'f5');
-    //   container.style.width = '260px';
-    //   details.appendChild(summary);
-  
-    var div = document.createElement('div');
-    div.setAttribute('class', 'post');
-    div.innerHTML = `
-          <div class="parent">
-             <div id="editor"></div>
-          </div>
-      `;
-  
-    const positionMarker = document.querySelector('.discussion-timeline-actions');
-    if (positionMarker) {
-      positionMarker.before(div);
-    } else {
-      console.log('Dashboard extension: position marker not found.');
+  const div = document.createElement('div');
+  div.setAttribute('class', 'post');
+  div.appendChild(createTextEditorBtn());
+  div.appendChild(createEditor());
+  div.appendChild(closeTextEditorBtn());
+
+  const positionMarker = document.querySelector('.js-comment-body');
+  if (positionMarker) {
+    positionMarker.after(div);
+  } else {
+    console.log('Dashboard extension: position marker not found.');
+  }
+}
+init();
+const editor = new Quill('#editor', {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['code-block', { list: 'ordered' }, { list: 'bullet' }]
+    ]
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow'  // or 'bubble'
+});
+
+editor.on('editor-change', function (eventName, range, oldRange, source) {
+  if (eventName === 'text-change') {
+  } else if (eventName === 'selection-change') {
+    if (!range) {
+      console.log(editor.root.innerHTML)
+      editor.root.innerHTML = '<p>sdfdddddgh<strong>dfghdfg</strong></p>'
     }
   }
-  init();
-  const editor  = new Quill('#editor', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['code-block', { list: 'ordered' }, { list: 'bullet' }]
-      ]
-    },
-    placeholder: 'Compose an epic...',
-    theme: 'snow'  // or 'bubble'
-  });
+});
