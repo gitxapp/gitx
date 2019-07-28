1396e659e4a1fdf2c2b9faead94d78844c77c751
Karthik Kumar <Karthik S Kumar>
2019-07-28 13:24:58 +0530

    code cleanup, add es lint

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@ function onInputValueChange(e) {
 // Load main input area and add some behaviors
 function initInputArea() {
   const textArea = document.getElementById('new_comment_field');
-  textArea.addEventListener('onblur', e => {
+  textArea.addEventListener('onblur', (e) => {
     console.log('Value', e.target.value);
   });
-  textArea.addEventListener('change', e => {
+  textArea.addEventListener('change', (e) => {
     onInputValueChange(e);
   });
-  textArea.addEventListener('input', e => {
+  textArea.addEventListener('input', (e) => {
     onInputValueChange(e);
   });
 }
@@ -31,33 +31,13 @@ function initInputArea() {
 function createPrivateNoteAddButton() {
   const button = document.createElement('button');
   button.textContent = 'Add private notes';
-  button.id = `add_private_note_button`;
+  button.id = 'add_private_note_button';
   button.type = 'button';
   button.classList.add('btn');
   button.classList.add('btn-primary');
   button.disabled = true;
-  button.onclick = function(e) {
-    document.getElementById(`parent${e.target.value}`).style.display = 'block';
-    document.getElementById(`close_private_note_button${e.target.value}`).style.display = 'block';
-    button.style.display = 'none';
-  };
-  return button;
-}
-
-// Create close private note  button
-
-function createPrivateNoteCloseButton() {
-  const button = document.createElement('button');
-  button.textContent = 'Save notes';
-  button.id = 'close_private_note_button';
-  button.type = 'button';
-  button.classList.add('pvt-cmt-btn');
-  button.classList.add('close-editor');
-  button.style.display = 'none';
-  button.onclick = function(e) {
-    document.getElementById(`parent${e.target.value}`).style.display = 'none';
-    document.getElementById(`add_private_note_button${e.target.value}`).style.display = 'block';
-    button.style.display = 'none';
+  button.onclick = () => {
+    button.disabled = true;
   };
   return button;
 }
@@ -66,11 +46,7 @@ function init() {
   initInputArea();
   const positionMarker = document.getElementById('partial-new-comment-form-actions');
   if (positionMarker) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'post');
-    div.appendChild(createPrivateNoteAddButton());
-    div.appendChild(createPrivateNoteCloseButton());
-    positionMarker.prepend(div);
+    positionMarker.prepend(createPrivateNoteAddButton());
     createNoteBox();
   } else {
     console.log('Extension: position marker not found.');
--- src/noteBox.js
+++ src/noteBox.js
@@ -1,41 +1,3 @@
-export default function createNoteBox() {
-  const commentBody = document.querySelector('#discussion_bucket > div.col-9 > div > div.js-discussion.js-socket-channel');
-  const note = document.createElement('div');
-
-  note.classList = ['timeline-comment-wrapper js-comment-container'];
-  note.appendChild(createAvatar());
-  note.appendChild(createCommentBox());
-  // note.innerHTML = '<p>sssp</p>'
-  document
-    .querySelector(`#discussion_bucket > div.col-9 > div > div.js-discussion.js-socket-channel > div:nth-child(${commentBody.children.length - 1})`)
-    .after(note);
-}
-
-function createAvatar() {
-  const avatarWrapper = document.createElement('div');
-  avatarWrapper.classList = ['avatar-parent-child timeline-comment-avatar'];
-
-  //a tag
-  const avatarA = document.createElement('a');
-  avatarA.href = 'www.google.com'; //change this
-  avatarA.classList = ['d-inline-block'];
-  avatarA.setAttribute('data-hovercard-type', 'user');
-  avatarA.setAttribute('data-hovercard-url', '/hovercards?user_id=22343433'); //change this
-  avatarA.setAttribute('data-octo-click', 'hovercard-link-click');
-  avatarA.setAttribute('data-octo-dimensions', 'link_type:self');
-
-  //image tag
-  const avatarImg = document.createElement('img');
-  avatarImg.classList = ['avatar rounded-1'];
-  avatarImg.height = '44';
-  avatarImg.width = '44';
-  avatarImg.alt = '@karthikvillanchira'; // change this
-  (avatarImg.src = 'https://avatars1.githubusercontent.com/u/22343433?s=180&v=4'), //chnage this
-    avatarA.appendChild(avatarImg);
-  avatarWrapper.appendChild(avatarA);
-  return avatarWrapper;
-}
-
 function createCommentBox() {
   const wrapper = document.createElement('div');
   wrapper.classList = ['timeline-comment-group js-minimizable-comment-group js-targetable-comment'];
@@ -47,7 +9,7 @@ function createCommentBox() {
   timelineWrapper.classList = ['timeline-comment-header clearfix'];
   const timelineH3 = document.createElement('h3');
   timelineH3.classList = ['timeline-comment-header-text f5 text-normal'];
-  timelineH3.innerHTML = `<strong class="css-truncate expandable"><a class="author text-inherit css-truncate-target">karthikvillanchira</a></strong>`;
+  timelineH3.innerHTML = '<strong class="css-truncate expandable"><a class="author text-inherit css-truncate-target">karthikvillanchira</a></strong>';
   timelineWrapper.append(timelineH3);
 
   // wip
@@ -67,7 +29,7 @@ function createCommentBox() {
   tr.classList = ['d-block'];
   const td = document.createElement('td');
   td.classList = ['d-block comment-body markdown-body  js-comment-body'];
-  td.innerHTML = `<p>settings</p>`;
+  td.innerHTML = '<p>settings</p>';
   tr.appendChild(td);
   tbody.appendChild(tr);
   table.appendChild(tbody);
@@ -76,4 +38,43 @@ function createCommentBox() {
   content.append(commentBodyWrapper);
   wrapper.append(content);
   return wrapper;
+}
+
+function createAvatar() {
+  const avatarWrapper = document.createElement('div');
+  avatarWrapper.classList = ['avatar-parent-child timeline-comment-avatar'];
+
+  // a tag
+  const avatarA = document.createElement('a');
+  avatarA.href = 'www.google.com'; // change this
+  avatarA.classList = ['d-inline-block'];
+  avatarA.setAttribute('data-hovercard-type', 'user');
+  avatarA.setAttribute('data-hovercard-url', '/hovercards?user_id=22343433'); // change this
+  avatarA.setAttribute('data-octo-click', 'hovercard-link-click');
+  avatarA.setAttribute('data-octo-dimensions', 'link_type:self');
+
+  // image tag
+  const avatarImg = document.createElement('img');
+  avatarImg.classList = ['avatar rounded-1'];
+  avatarImg.height = '44';
+  avatarImg.width = '44';
+  avatarImg.alt = '@karthikvillanchira'; // change this
+  avatarImg.src = 'https://avatars1.githubusercontent.com/u/22343433?s=180&v=4'; // chnage this
+  avatarA.appendChild(avatarImg);
+  avatarWrapper.appendChild(avatarA);
+  return avatarWrapper;
+}
+
+
+export default function createNoteBox() {
+  const commentBody = document.querySelector('#discussion_bucket > div.col-9 > div > div.js-discussion.js-socket-channel');
+  const note = document.createElement('div');
+
+  note.classList = ['timeline-comment-wrapper js-comment-container'];
+  note.appendChild(createAvatar());
+  note.appendChild(createCommentBox());
+  // note.innerHTML = '<p>sssp</p>'
+  document
+    .querySelector(`#discussion_bucket > div.col-9 > div > div.js-discussion.js-socket-channel > div:nth-child(${commentBody.children.length - 1})`)
+    .after(note);
 }
