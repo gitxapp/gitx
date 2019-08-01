9b88eb3a049a2ebe64897d3185919915a2c8f3bb
Muhsin.K <muhsinkeramam@gmail.com>
2019-08-01 10:06:50 +0530

    Note flow (#4)
    * WIP: note flow
    
    * WIP: note flow

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import minAjax from './ajax';
 import { URL } from './constants';
 import './style.css';
 
-const userId = '5d4014afee31519f2dbcecd2';
+const userId = '5d4145e999c6db6b9d23380b';
 let note = null;
 let allNotes = [];
 
-function loadAllTheNotes() {
-  minAjax({
-    url: `${URL}note/${userId}`, // request URL
-    type: 'GET', // Request type GET/POST
-    success(data) {
-      allNotes = JSON.parse(data);
-      console.log('All Notes hi watching', allNotes);
-    },
-  });
-}
+let nearest_comment_id = null;
+// Check the url is issue or pull
+const currentUrl = document.location.toString().toLowerCase();
+const urlParams = currentUrl.split('/');
+const issue_id = urlParams[urlParams.length - 1];
+const note_type = currentUrl.includes('issue') ? 'issue' : 'pull';
+
 // Disable/Enable Add private button based on value entered
 
 function onInputValueChange(e) {
@@ -26,20 +23,20 @@ function onInputValueChange(e) {
   note = e.target.value;
 
   if (e.target.value.length > 0) {
-    addPrivateNoteButton.disabled = false;
+    // addPrivateNoteButton.disabled = false;
   } else {
-    addPrivateNoteButton.disabled = true;
+    // addPrivateNoteButton.disabled = true;
   }
 }
 
 // Load main input area and add some behaviors
 function initInputArea() {
   const textArea = document.getElementById('new_comment_field');
-  textArea.addEventListener('onblur', (e) => {});
-  textArea.addEventListener('change', (e) => {
+  textArea.addEventListener('onblur', e => {});
+  textArea.addEventListener('change', e => {
     onInputValueChange(e);
   });
-  textArea.addEventListener('input', (e) => {
+  textArea.addEventListener('input', e => {
     onInputValueChange(e);
   });
 }
@@ -52,16 +49,29 @@ function createPrivateNoteAddButton() {
   button.type = 'button';
   button.classList.add('btn');
   button.classList.add('btn-primary');
-  button.disabled = true;
-  button.onclick = (e) => {
-    button.disabled = true;
+  // button.disabled = true;
+  button.onclick = e => {
+    // button.disabled = true;
+    const commentBoxes = document.querySelectorAll('.js-comment-container:not(.private-note)');
+    const commentBoxCount = commentBoxes.length;
+    console.log('commentBoxes', commentBoxes);
+
+    // Find nearest comment id
+    const nearestBox = commentBoxes[commentBoxCount - 2].querySelector('.js-comment-container [id]').id;
+    nearest_comment_id = nearestBox.split('-').pop();
+    console.log('Issue Id', issue_id);
+    console.log('Comment Id', nearest_comment_id);
+
     minAjax({
-      url: `${URL }note`, // request URL
+      url: `${URL}note`, // request URL
       type: 'POST', // Request type GET/POST
       data: {
         user_id: userId,
         note_content: note,
-        type: 'issue',
+        type: note_type,
+        issue_id,
+        nearest_comment_id,
+        nearest_created_date: new Date(),
       },
       success(data) {},
     });
@@ -73,18 +83,36 @@ function init() {
   initInputArea();
   const positionMarker = document.getElementById('partial-new-comment-form-actions');
   if (positionMarker) {
-    positionMarker.prepend(createPrivateNoteAddButton());
-    const commentBoxes = Array.from(document.getElementsByClassName('js-comment-container'));
+    minAjax({
+      url: `${URL}note/${userId}`, // request URL
+      type: 'GET', // Request type GET/POST
+      success(data) {
+        allNotes = JSON.parse(data);
+        positionMarker.prepend(createPrivateNoteAddButton());
+        // console.log('All Notes ... -->', allNotes);
+        const commentBoxes = document.querySelectorAll('.js-comment-container');
+        commentBoxes.forEach((commentBox, index) => {
+          const commentBoxId = commentBox.querySelector('.timeline-comment-group');
+          if (commentBoxId) {
+            const commentId = commentBoxId.id.split('-').pop();
 
-    commentBoxes.forEach((commentBox, index) => {
-      // prevent injection in text box in the last
-      if (index < commentBoxes.length - 1) {
-        commentBox.after(createNoteBox(index));
-        document.getElementById(`comment-box-${index}`).addEventListener('click', (e) => {
-          // delete functionlity of comment boxes
-          console.log(index);
+            const checkComment = obj => {
+              return obj.nearest_comment_id === commentId;
+            };
+            const checkCommentExist = allNotes.filter(checkComment);
+            console.log('checkCommentExist', checkCommentExist);
+
+            checkCommentExist.reverse().forEach(element => {
+              commentBox.after(createNoteBox(index, element));
+            });
+          }
+
+          // document.getElementById(`comment-box-${index}`).addEventListener('click', e => {
+          // delete functionality of comment boxes
+          //   console.log(index);
+          // });
         });
-      }
+      },
     });
   } else {
     console.log('Extension: position marker not found.');
@@ -93,4 +121,4 @@ function init() {
 window.onload = () => {
   init();
 };
-loadAllTheNotes();
+// loadAllTheNotes();
