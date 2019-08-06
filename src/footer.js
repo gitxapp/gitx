93f0c68a29a2a427408a1b44f063a26e62cabd13
Muhzi4u <muhsinkeramam@gmail.com>
2019-08-06 17:35:42 +0530

    update footer

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,10 @@
 function createFooter() {
   const node = document.createElement('div');
   node.innerHTML = `<div class="private-note-status" style="display: block;"><div class="sign-in">
-    <a title="" class="sign-in-now" href="www.google.com" target="_blank" >Sign in now</a> to use <b>GitEx</b>
+    <a title="" class="sign-in-now" href="https://github.com/login/oauth/authorize?client_id=532649fddafad8da7008&redirect_uri=http://localhost:5000/api/v1/oauth/redirect" target="_blank" >Sign in now</a> to use <b>GitEx</b>
   </div>
   <span class="signing-in hide">
-    Signing in...
+    
   </span>
   </div>`;
   const body = document.getElementsByTagName('BODY');
