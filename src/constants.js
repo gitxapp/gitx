7d7e15098a137dc16001f339a3b2c619e0839177
Muhzi4u <muhsinkeramam@gmail.com>
2019-08-30 21:36:35 +0530

    chnage constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 // export const URL = 'http://localhost:5000/api/';
+// export const REDIRECT_URL = 'http://localhost:5000/api/v1/oauth/redirect';
+
 export const URL = 'https://morning-headland-55324.herokuapp.com/api/';
+export const REDIRECT_URL = 'https://morning-headland-55324.herokuapp.com/api/v1/oauth/redirect';
 
 export const VERSION = 'v1';
 
 export const APP_ID = '2bcec029b6dfb5dc3c64';
-export const REDIRECT_URL = 'https://morning-headland-55324.herokuapp.com/api/v1/oauth/redirect';
