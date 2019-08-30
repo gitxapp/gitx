18d6710e2a593c1df8c1d0e7d967f09d71dfcb5b
Muhzi4u <muhsinkeramam@gmail.com>
2019-08-30 21:41:10 +0530

    release chrome extension beta version 0.0.2

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 // export const URL = 'http://localhost:5000/api/';
 // export const REDIRECT_URL = 'http://localhost:5000/api/v1/oauth/redirect';
 
-export const URL = 'https://morning-headland-55324.herokuapp.com/api/';
-export const REDIRECT_URL = 'https://morning-headland-55324.herokuapp.com/api/v1/oauth/redirect';
+export const URL = 'https://gitextended.herokuapp.com/api/';
+export const REDIRECT_URL = 'https://gitextended.herokuapp.com/api/v1/oauth/redirect';
 
 export const VERSION = 'v1';
 
