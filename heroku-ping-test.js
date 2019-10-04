9855ab6e184943609a93c9b6c5da0fa4394856a9
muhzi <muhsinkeramam@gmail.com>
2019-10-04 11:36:53 +0530

    add heroku ping test file

---
+++ heroku-ping-test.js
@@ -0,0 +1,28 @@
+import http from 'http';
+
+const HEROKU_URL = 'gitextended.herokuapp.com';
+
+function startKeepAlive() {
+  setInterval(() => {
+    const options = {
+      host: HEROKU_URL,
+      port: 80,
+      path: '/',
+    };
+    http
+      .get(options, res => {
+        res.on('data', chunk => {
+          try {
+            // optional logging... disable after it's working
+            console.log(`HEROKU RESPONSE: ${chunk}`);
+          } catch (err) {
+            console.log(err.message);
+          }
+        });
+      })
+      .on('error', err => {
+        console.log(`Error: ${err.message}`);
+      });
+  }, 1 * 60 * 1000); // load every 20 minutes
+}
+startKeepAlive();
