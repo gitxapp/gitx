1f02d229d7a2cd45f9941ee3fbefe5b6a74c5547
muhzi <muhsinkeramam@gmail.com>
2020-03-05 13:15:39 +0530

    Update some env variables and constastns

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,15 @@
-export const VERSION = 'v1';
+export const VERSION = "v1";
 
-export const INSTALL_URL = 'https://gitxapp.com/connect.html';
-export const UN_INSTALL_URL = 'https://gitxapp.com/uninstall.html';
-export const URL = 'https://gitextended.herokuapp.com/api/';
-export const REDIRECT_URL = 'https://gitextended.herokuapp.com/api/v1/oauth/redirect';
+export const INSTALL_URL = "https://gitxapp.com/connect.html";
+export const UN_INSTALL_URL = "https://gitxapp.com/uninstall.html";
 
-export const APP_ID = '2bcec029b6dfb5dc3c64';
+// Production
 
-// For testing in local machine
-// export const URL = 'http://localhost:5000/api/';
-// export const REDIRECT_URL = 'http://localhost:5000/api/v1/oauth/redirect';
-// export const APP_ID = '532649fddafad8da7008';
+// export const URL = 'https://gitextended.herokuapp.com/api/';
+// export const REDIRECT_URL = 'https://gitextended.herokuapp.com/api/v1/oauth/redirect';
+// export const APP_ID = '2bcec029b6dfb5dc3c64';
+
+// Local host
+export const URL = "http://localhost:5000/api/";
+export const REDIRECT_URL = "http://localhost:5000/api/v1/oauth/redirect";
+export const APP_ID = "532649fddafad8da7008";
