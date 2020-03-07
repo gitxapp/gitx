f1959b2d2d48144343bd4b62cfb216e579ac2d5f
muhzi <muhsinkeramam@gmail.com>
2020-03-07 16:05:48 +0530

    Add helper for getting all the users organizations

---
+++ server/utils/constants.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const REPO_OWNER_TYPES = {
+  ORGANIZATION: "organization",
+  USER: "user"
+};
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,16 +1,46 @@
-import axios from 'axios';
+/* eslint-disable consistent-return */
+import axios from "axios";
+import { REPO_OWNER_TYPES } from "./constants";
 
-const getRepoOwnerType = async (name) => {
+const getRepoOwnerType = async ({ repoOwner }) => {
+  if (!repoOwner) throw Error("Invalid argument");
+  const apiUrl = `${process.env.GITHUB_API_URL}orgs/${repoOwner}`;
 
-    if (!name) throw Error('Invalid argument')
-
-    try {
-        let result = await axios.get(`${process.env.GITHUB_API_URL}orgs/${name}`);
-        if(result.id) return 'organization'
-        return 'user'
-    } catch (e) {
-        throw Error('Internal error')
+  try {
+    const result = await axios.get(apiUrl);
+    if (result) {
+      return REPO_OWNER_TYPES.ORGANIZATION;
     }
-}
+  } catch (e) {
+    const { response } = e;
 
-export { getRepoOwnerType };
+    if (response) {
+      return REPO_OWNER_TYPES.USER;
+    }
+
+    throw Error("Internal error");
+  }
+};
+
+const getUserOrganizations = async ({ userName }) => {
+  if (!userName) throw Error("Invalid argument");
+  const apiUrl = `${process.env.GITHUB_API_URL}users/${userName}/orgs`;
+
+  try {
+    const result = await axios.get(apiUrl);
+
+    const { data } = result;
+    return data;
+  } catch (e) {
+    const { response } = e;
+
+    if (response) {
+      return REPO_OWNER_TYPES.USER;
+    }
+
+    throw Error("Internal error");
+  }
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { getRepoOwnerType, getUserOrganizations };
